3e51be790a6db87276dca793c0c8387c2322dfa5
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-03 08:39:04 +0530

    Frontend add headings

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,20 @@
+echo Installing Nginx
 dnf install nginx -y
+
+echo Copy expense config file
 cp expense.conf /etc/nginx/default.d/expense.conf
+
+echo clean old content
 rm -rf /usr/share/nginx/html/*
+
+echo download frontend content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
+
+echo extract content
 unzip /tmp/frontend.zip
+
+echo restart service
 systemctl restart nginx
 systemctl enable nginx
+
