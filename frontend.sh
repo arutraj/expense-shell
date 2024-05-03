03d057c6fbcf794a449c5f439750596c4e58f655
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-03 08:07:55 +0530

    Troubleshooting frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,5 @@
 dnf install nginx -y
+cp expense.conf /etc/nginx/default.d/expense.conf
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
@@ -6,7 +7,4 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-ls
-pwd
-cp expense.conf /etc/nginx/default.d/expense.conf
 systemctl restart nginx
