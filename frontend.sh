cae0286e98d06dc489e1f519e17be4b3660326a4
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-04 08:52:00 +0530

    DRY code frontend

--- frontend.sh
+++ frontend.sh
@@ -8,24 +8,24 @@ HEADING Installing Nginx
 dnf install nginx -y &>>/tmp/expense.log
 echo exit status - $?
 
-Copy expense config file
+HEADING Copy expense config file
 cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 echo exit status - $?
 
-clean old content
+HEADING clean old content
 rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 echo exit status - $?
 
-download frontend content
+HEADING download frontend content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/expense.log
 echo exit status - $?
 cd /usr/share/nginx/html
 
-extract content
+HEADING extract content
 unzip /tmp/frontend.zip &>>/tmp/expense.log
 echo exit status - $?
 
-restart service
+HEADING restart service
 systemctl enable nginx &>>/tmp/expense.log
 systemctl restart nginx &>>/tmp/expense.log
 echo exit status - $?
