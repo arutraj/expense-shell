45fda801353577d0262cf96567985906f4cc000e
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-04 08:06:25 +0530

    MySQL remove hardcoded password.

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,12 @@
-echo -e "\e[35mInstalling Nginx\e[0m"
+echo -e "\e[35mInstalling MySQL Server\e[0m"
 dnf install mysql-server -y
 echo exit status - $?
 
-echo -e "\e[35mInstalling Nginx\e[0m"
+echo -e "\e[35Start Nginx Service\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
 echo exit status - $?
 
-echo -e "\e[35mInstalling Nginx\e[0m"
+echo -e "\e[35mSet Root Password\e[0m"
 mysql_secure_installation --set-root-pass $1
 echo exit status - $?
