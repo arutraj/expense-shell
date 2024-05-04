441fa078360e3ac3dba964d5042735f2295c5dca
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-04 08:58:10 +0530

    Adding common script for keeping code DRy

--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,15 @@
 source common.sh
 rm -f /tmp/expense.log
 
-echo -e "\e[35mInstalling MySQL Server\e[0m"
+HEADING Installing MySQL Server
 dnf install mysql-server -y
 echo exit status - $?
 
-echo -e "\e[35mStart Nginx Service\e[0m"
+HEADING Start Nginx Service
 systemctl enable mysqld
 systemctl start mysqld
 echo exit status - $?
 
-echo -e "\e[35mSet Root Password\e[0m"
+HEADING Set Root Password
 mysql_secure_installation --set-root-pass $1
 echo exit status - $?
