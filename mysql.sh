f4e3ae74fac622b30f6c2745f8a9ae908524e289
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-06 08:17:58 +0530

    Exit Message print rather than code.

--- mysql.sh
+++ mysql.sh
@@ -3,18 +3,34 @@ rm -f /tmp/expense.log
 
 if [ -z "$1" ]; then
   echo "INput MySQL Password is Missing"
-  exit
+  exit 1
 fi
 
 HEADING Installing MySQL Server
 dnf install mysql-server -y
-echo exit status - $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 2
+fi
 
 HEADING Start Nginx Service
 systemctl enable mysqld
 systemctl start mysqld
-echo exit status - $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 2
+fi
 
 HEADING Set Root Password
 mysql_secure_installation --set-root-pass $1
-echo exit status - $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 2
+fi
+
